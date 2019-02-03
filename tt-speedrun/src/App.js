4c49df212ed5f5db94827d093e640891bdaa6e26
JasonCorp84 <csababalogh84@gmail.com>
2019-02-02 19:04:31 -0800

    board simplified

--- tt-speedrun/src/App.js
+++ tt-speedrun/src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
-import Board from './Board';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: Array(9).fill(null)
+    }
+  }
+
+  handleClick (e) {
+    console.log(e.target);
+  }
+
   render() {
+
+   const box = this.state.board.map(
+    (box,i) =>
+    <div key={i} 
+    className="box" 
+    onClick={(e) => this.handleClick(e)}>
+    {box}
+    </div>)
+
     return (
-      <div className="App">
-        <Board />
+      <div className="container">
+      <h1>Tic Tac Toe</h1>
+        <div className="board">
+          {box}
+        </div>
       </div>
     );
   }
