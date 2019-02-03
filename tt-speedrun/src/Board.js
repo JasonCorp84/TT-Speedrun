ec3777b0833b019db4a5b40ca234654c1fea6d92
JasonCorp84 <csababalogh84@gmail.com>
2019-02-02 18:12:44 -0800

    board placeholder added

--- tt-speedrun/src/Board.js
+++ tt-speedrun/src/Board.js
@@ -3,12 +3,25 @@ import React, { Component } from 'react';
 class Board extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {
+      row1: ['','',''],
+      row2: ['','',''],
+      row3: ['','','']
+      }
   }
   render() { 
+    const style = {
+      height: '100px',
+      border: "1px solid black",
+      display: 'inline',
+      float: 'left',
+      width: '33%'
+    }
     return ( 
-      <div>
-        BOARD
+      <div style={{width: '50%', justifySelf: 'center'}}>
+        {this.state.row1.map(e => <div style={style}>{e}</div>)}
+        {this.state.row2.map(e => <div style={style}>{e}</div>)}
+        {this.state.row3.map(e => <div style={style}>{e}</div>)}
       </div>
      );
   }
